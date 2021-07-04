2dfbba6cc170b57369a8db5d6ae39581af50ce52
Виктор <basinvm@gmail.com>
2021-07-04 15:39:29 +0300

    обновления

---
+++ lesson_5/task_3.py
@@ -0,0 +1,17 @@
+print ("1-треугольник, 2-квадрат")
+figure = input ("Выберите фигуру: ")
+if figure == '1':
+    print ("Длины сторон треугольника:")
+    a = float (input ("a = "))
+    b = float (input ("b = "))
+    c = float (input ("c = "))
+    p = (a + b + c) / 2
+    from math import sqrt
+    s = sqrt(p * (p - a) * (p - b) * (p - c))
+    print("Площадь: %.2f" % s)
+elif figure == '2':
+    print("Длина сторон квадрата:")
+    a = float(input ("a = "))
+    print("Площадь: %.2f" % (a * a))
+else:
+    print("Не правильный ввод.\nВыберите из списка, от 1 - 2")
