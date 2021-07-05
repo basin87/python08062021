e062beeeb0fba64caf01ab5b3c8e49ba92b8c573
Виктор <basinvm@gmail.com>
2021-07-05 13:38:36 +0300

    обновления

--- lesson_4/task_3.py
+++ lesson_4/task_3.py
@@ -1,11 +1,13 @@
-def int_numbers():
-    a = int(input("Введите целое число A: "))
-    b = int(input("Введите целое число B: "))
+def int_numbers(a,b):
+
     if a < b:
         for i in range(a, b + 1):
-            print(i)
+            yield i
     else:
         for i in range(a, b - 1, -1):
-            print(i)
-    return()
-int_numbers()
+            yield i
+
+a1 = int(input("Введите целое число A: "))
+b1 = int(input("Введите целое число B: "))
+for num in int_numbers(a1,b1):
+    print(num)
